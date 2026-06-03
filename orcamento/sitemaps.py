b58a01882eceae72b869b64b11a9f7c6adcc6e2c
ArthurLLJ <arthur.garciacr7@gmail.com>
2026-06-03 10:56:00 -0300

    Adiciona sitemap

---
+++ orcamento/sitemaps.py
@@ -0,0 +1,21 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
+class StaticViewSitemap(Sitemap):
+
+    priority = 0.8
+    changefreq = 'weekly'
+
+    def items(self):
+        return [
+            'index',
+            'produtos',
+            'orcamento',
+            'localizacao',
+            'sobre',
+            'contato',
+        ]
+
+    def location(self, item):
+        return reverse(item)
--- projeto/urls.py
+++ projeto/urls.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from orcamento.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('orcamento.urls')),
+
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    ),
 ]
